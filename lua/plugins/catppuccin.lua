f6ed3fb5d6a5960f6d27b01742b22eacb5f86724
LurkerP <henriquegou310822@gmail.com>
2024-02-26 20:11:21 -0300

    Fix: Indentation

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,15 +1,15 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	lazy = false,
-	config = function()
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    config = function()
         local catppuccin = require("catppuccin")
 
         catppuccin.setup({
             transparent_background = true
         })
 
-		vim.cmd('colorscheme catppuccin')
-	end
+        vim.cmd('colorscheme catppuccin')
+    end
 }
