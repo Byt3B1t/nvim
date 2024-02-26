baba1ab27a9c53917fa9c23abe7726f71b29b27d
LurkerP <henriquegou310822@gmail.com>
2024-02-26 20:08:16 -0300

    Add: Custom Menu

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,31 +1,40 @@
 return {
-  "goolord/alpha-nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
+    "goolord/alpha-nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
 
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.startify")
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
 
-    dashboard.section.header.val = {
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                     ]],
-      [[       ████ ██████           █████      ██                     ]],
-      [[      ███████████             █████                             ]],
-      [[      █████████ ███████████████████ ███   ███████████   ]],
-      [[     █████████  ███    █████████████ █████ ██████████████   ]],
-      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-    }
+        dashboard.section.header.val = {
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                     ]],
+            [[       ████ ██████           █████      ██                     ]],
+            [[      ███████████             █████                             ]],
+            [[      █████████ ███████████████████ ███   ███████████   ]],
+            [[     █████████  ███    █████████████ █████ ██████████████   ]],
+            [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+            [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+            [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+        }
 
-    alpha.setup(dashboard.opts)
-  end,
+        dashboard.section.buttons.val = {
+            dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("<leader> p f", "󰈞  Find file", ":Telescope find_files <CR>"),
+            dashboard.button("<leader> p r", "󰊄  Recently opened files", ":Telescope oldfiles <CR>"),
+            dashboard.button("<leader> p s", "󰈬  Find word", ":Telescope live_grep <CR>"),
+            dashboard.button("<leader> c c", "  Configuration", ":e ~/.config/nvim/<CR>"),
+
+        }
+
+        alpha.setup(dashboard.opts)
+    end,
 }
