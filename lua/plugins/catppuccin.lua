7f62c0259a14a75147c77817caee8a31313decd6
LurkerP <henriquegou310822@gmail.com>
2024-02-26 20:08:57 -0300

    Add: Enable Transparency

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,6 +4,12 @@ return {
 	priority = 1000,
 	lazy = false,
 	config = function()
+        local catppuccin = require("catppuccin")
+
+        catppuccin.setup({
+            transparent_background = true
+        })
+
 		vim.cmd('colorscheme catppuccin')
 	end
 }
